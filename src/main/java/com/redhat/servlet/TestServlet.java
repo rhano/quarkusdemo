df2ebcd4a074da71e30dfc7805b055fd45822a2e
rhano <rhano@rhano.remote.csb>
2022-04-08 12:33:55 -0400

    first commit

---
+++ src/main/java/com/redhat/servlet/TestServlet.java
@@ -0,0 +1,16 @@
+package com.redhat.servlet;
+import javax.servlet.http.*;
+import javax.servlet.*;
+import java.io.*;
+
+public class TestServlet extends HttpServlet{
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException,IOException
+    {
+        res.setContentType("text/html");//setting the content type
+        PrintWriter pw=res.getWriter();//get the stream to write the data
+
+//writing html in the stream
+        String name=req.getParameter("name");
+        pw.println("Welcome "+name);
+    }}
