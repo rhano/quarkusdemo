ab8923cc75b8d232d695d6dfac477c3a328d07de
rhano <rhano@rhano.remote.csb>
2022-04-08 16:53:28 -0400

    comitting new message

--- src/main/java/com/redhat/servlet/TestServlet.java
+++ src/main/java/com/redhat/servlet/TestServlet.java
@@ -18,6 +18,6 @@ public class TestServlet extends HttpServlet {
         pw.print("<html><head><link rel=\"stylesheet\" href=\"css/style.css\"></head>");
         String name = req.getParameter("name");
         System.out.println(name);
-        pw.println("<div class=\"container\"><center><img src=\"images/quarkus.png\" class=\"rotate\" width=\"100\" height=\"100\" /></center><br/><span><font size='20' color='white'>Welcome " + name + "!</font></span><div><br/><center><img src=\"images/openshift.jpg\" class=\"rotate\" width=\"100\" height=\"100\" /></center></html>");
+        pw.println("<div class=\"container\"><center><img src=\"images/quarkus.png\" class=\"rotate\" width=\"200\" height=\"200\" /></center><br/><br/><span><font size='20' color='white'>Welcome " + name + "!</font></span><div><br/><br/><center><img src=\"images/openshift.jpg\" class=\"rotate\" width=\"200\" height=\"200\" /></center></html>");
     }
 }
